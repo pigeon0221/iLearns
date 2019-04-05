2da6df4846d29a322bf9197db83d3e7cf2e33455
Alex <alexlazar21@gmail.com>
2019-04-05 17:41:16 -0400

    Reader code to convert to values

--- ILearnsGUI/Conference/conferenceRFID.py
+++ ILearnsGUI/Conference/conferenceRFID.py
@@ -2,8 +2,10 @@ import serial
 import sys
 import platform
 
-output_File_Clear = open('output.txt', 'w').close()
+open('output.txt', 'w').close()
 output_File = open('output.txt', 'a')
+dictionary = open('dictionary.txt', 'a')
+rfidValue={}
 print(platform.system())
 ser=None
 
@@ -27,9 +29,38 @@ if platform.system() is 'Windows':
         if('USB' in p.description):
             ser = serial.Serial(p.description, 9600)
 
-
+mode = int(input("Enter 1 for READ Mode , Enter 2 for SETUP --- "))
+clear = None
+if(mode == 1):
+    lines = None;
+    list=[]
+    with open('dictionary.txt') as f:
+        lines = f.readlines()
+    for l in lines:
+        if(len(l[:len(l)-1])!=0):
+            list.append(l[:len(l)-1])
+    for x in range(0,len(list),2):
+        rfidValue[list[x]]=list[x+1]
+if(mode == 2):
+    clear = int(input("Enter 1 to clear dictionary , Enter 2 to add new keys --- "))
 while True:
     data = ser.readline()
+    # READ Mode outputs information when an input is recieved
+    # SETUP Mode allows the user to input new tags and there information.
     if data:
-        print("Recieved KEY = " + str(data, 'utf-8'));
-
+        if (mode == 1):
+            key = str(data, 'utf-8').split(':')[1]
+            key=(key[:len(key)-1])
+            print("Recieved KEY = " + key)
+            print("This key = "+rfidValue[key])
+        if (mode == 2):
+            if(clear==1):
+                open('dictionary.txt', 'w').close()
+            if(clear==2):
+                dictionary=open('dictionary.txt', 'a')
+                dictionary.write(str(data, 'utf-8').split(':')[1]+'\n')
+                dictionary.close()
+                dictionary = open('dictionary.txt', 'a')
+                value=str(input("What do you want this to be?")+'\n')
+                dictionary.write(value)
+                dictionary.close()
