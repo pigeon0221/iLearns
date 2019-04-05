4e1c55548caeab1fe064e6a2c94c30b01bd546df
rwasenge <mrwasengevm@gmail.com>
2019-04-05 17:41:35 -0400

    tick tack toe board created

--- ILearnsGUI/src/Main.java
+++ ILearnsGUI/src/Main.java
@@ -4,17 +4,43 @@ import processing.core.PApplet;
 import processing.core.PFont;
 import processing.core.PImage;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 
 
 public class Main extends PApplet{
-    private int exit_Button_X;
-    private int exit_Button_Y;
-    boolean begin = true;
-    boolean game_Menu=false;
+
+    int pos1x = 350;
+    int pos1y = 250;
+    int pos2x = 630;
+    int pos2y = 250;
+    int pos3x = 920;
+    int pos3y = 250;
+    int pos4x = 350;
+    int pos4y = 500;
+    int pos5x = 630;
+    int pos5y = 500;
+    int pos6x = 920;
+    int pos6y = 500;
+    int pos7x = 350;
+    int pos7y = 750;
+    int pos8x = 630;
+    int pos8y = 750;
+    int pos9x = 920;
+    int pos9y = 750;
+
+    char spot1 = 'X';
+    char spot2 = ' ';
+    char spot3 = 'O';
+    char spot4 = ' ';
+    char spot5 = 'X';
+    char spot6 = ' ';
+    char spot7 = 'O';
+    char spot8 = ' ';
+    char spot9 = 'X';
+
+
+
     public static void main(String[] args) {
         PApplet.main("Main");
         playMusic();
@@ -23,7 +49,7 @@ public class Main extends PApplet{
 
     private static void playMusic() {
         try {
-            FileInputStream fileInputStream = new FileInputStream("Kids_Music.mp3");
+            FileInputStream fileInputStream = new FileInputStream("farewell_226883231_soundcloud.mp3");
             Player player = new Player(fileInputStream);
             player.play();
 
@@ -38,8 +64,7 @@ public class Main extends PApplet{
 
     public void settings(){
         fullScreen();
-        exit_Button_X=displayWidth-150;
-        exit_Button_Y=0;
+
     }
 
     public void setup(){
@@ -48,118 +73,26 @@ public class Main extends PApplet{
     }
 
     public void draw(){
-        if(begin) {
-            load_Start_Screen();
-        }
-        if(game_Menu){
-           load_Game_Menu();
-        }
-
+        PImage background;
+        background=loadImage("black-and-white-board.jpg");
+        image(background,0,0,displayWidth,displayHeight);
+        PFont font = createFont("SqueakyChalkSound.ttf",200);
+        textFont(font);
+        
+        text(spot1,pos1x,pos1y);
+        text(spot2,pos2x,pos2y);
+        text(spot3,pos3x,pos3y);
+        text(spot4,pos4x,pos4y);
+        text(spot5,pos5x,pos5y);
+        text(spot6,pos6x,pos6y);
+        text(spot7,pos7x,pos7y);
+        text(spot8,pos8x,pos8y);
+        text(spot9,pos9x,pos9y);
     }
 
 
     public void mousePressed(){
-        try {
-            FileInputStream fileInputStream = new FileInputStream("button.mp3");
-            Player player = new Player(fileInputStream);
-            player.play();
-
-        }catch(FileNotFoundException e) {
-            e.printStackTrace();
-        }catch(JavaLayerException e) {
-            e.printStackTrace();
-        }
-        if(over_Exit()){
-            exit();
-        }
-        if(over_Start()){
-            begin=false;
-            game_Menu=true;
-        }
-    }
-    public boolean over_Exit(){
-        if(mouseX>displayWidth-150 && mouseY<100){
-            return true;
-        }
-        return false;
-    }
-    public boolean over_Start(){
-        if(mouseX>width/3 &&  mouseY>height/2-200 &&  mouseY<height/2-100){
-            return true;
-        }
-        return false;
-    }
-    public void load_Start_Screen(){
-
-        PImage img;
-        img = loadImage("cartoonB.png");
-        image(img,0,0,width,height);
-        fill(0);
-
-        rect(exit_Button_X,exit_Button_Y,150,100);
-        PImage exit;
-        exit = loadImage("Exit.png");
-        image(exit,exit_Button_X,exit_Button_Y,150,100);
-        PFont mono;
-        mono = createFont("SqueakyChalkSound.ttf",100);
-        textFont(mono);
-        fill(255);
-        text("iLearns",0,100);
-        text("Click here to begin",width/3,height/2-100);
-
-    } public void load_Game_Menu() {
-        BufferedReader reader;
-        String line;
-        reader = createReader("Python Setup/output.txt");
-
-
-        clear();
-
-        PImage img;
-        img = loadImage("cartoonB.png");
-        image(img, 0, 0, width, height);
-        fill(0);
-
-        rect(exit_Button_X, exit_Button_Y, 150, 100);
-        PImage exit;
-        exit = loadImage("Exit.png");
-        image(exit, exit_Button_X, exit_Button_Y, 150, 100);
-        PFont mono;
-        mono = createFont("SqueakyChalkSound.ttf", 100);
-        textFont(mono);
-        fill(255);
-        text("iLearns", 0, 100);
-
-
-        PImage dog;
-        dog = loadImage("dog.png");
-        image(dog, 1200, 550, 350, 350);
-
-        stroke(0);
-        strokeWeight(3);
-        line(700, 500, 900, 500);
-        line(1000, 500, 1200, 500);
-        line(1300, 500, 1500, 500);
-
-
-        try {
-            line = reader.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-            line = null;
-        }
-
-        if (line != null) {
-            char[] charArray = line.toCharArray();
-            int x=750;
-            for(char c :charArray){
-                text(c,x,480);
-                x+=500;
-            }
-
-        }
-
-
+        System.out.println(mouseX +" "+mouseY);
 
 
     }
