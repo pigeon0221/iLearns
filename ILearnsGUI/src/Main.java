09abf94cee18be17afe5e24fab632c8044d2a29e
rwasenge <mrwasengevm@gmail.com>
2019-04-06 18:51:03 -0400

    BOARD UPDATES FROM OUTPUT.TXT -NO WIN CODE YET

--- ILearnsGUI/src/Main.java
+++ ILearnsGUI/src/Main.java
@@ -4,8 +4,10 @@ import processing.core.PApplet;
 import processing.core.PFont;
 import processing.core.PImage;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class Main extends PApplet{
@@ -29,15 +31,15 @@ public class Main extends PApplet{
     int pos9x = 920;
     int pos9y = 750;
 
-    char spot1 = 'X';
+    char spot1 = ' ';
     char spot2 = ' ';
-    char spot3 = 'O';
+    char spot3 = ' ';
     char spot4 = ' ';
-    char spot5 = 'X';
+    char spot5 = ' ';
     char spot6 = ' ';
-    char spot7 = 'O';
+    char spot7 = ' ';
     char spot8 = ' ';
-    char spot9 = 'X';
+    char spot9 = ' ';
 
 
 
@@ -72,31 +74,87 @@ public class Main extends PApplet{
 
     }
 
-    public void draw(){
+    public void draw() {
         PImage background;
-        background=loadImage("black-and-white-board.jpg");
-        image(background,0,0,displayWidth,displayHeight);
-        PFont font = createFont("SqueakyChalkSound.ttf",200);
+        background = loadImage("black-and-white-board.jpg");
+        image(background, 0, 0, displayWidth, displayHeight);
+        PFont font = createFont("SqueakyChalkSound.ttf", 200);
         textFont(font);
 
-        text(spot1,pos1x,pos1y);
-        text(spot2,pos2x,pos2y);
-        text(spot3,pos3x,pos3y);
-        text(spot4,pos4x,pos4y);
-        text(spot5,pos5x,pos5y);
-        text(spot6,pos6x,pos6y);
-        text(spot7,pos7x,pos7y);
-        text(spot8,pos8x,pos8y);
-        text(spot9,pos9x,pos9y);
+//        text(spot1, pos1x, pos1y);
+//        text(spot2, pos2x, pos2y);
+//        text(spot3, pos3x, pos3y);
+//        text(spot4, pos4x, pos4y);
+//        text(spot5, pos5x, pos5y);
+//        text(spot6, pos6x, pos6y);
+//        text(spot7, pos7x, pos7y);
+//        text(spot8, pos8x, pos8y);
+//        text(spot9, pos9x, pos9y);
+
+        BufferedReader reader;
+        String line;
+        reader = createReader("output.txt");
+
+        try {
+            line = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+            line = null;
+        }
+
+        if (line != null) {
+            char[] charArray = line.toCharArray();
+            int x = charArray.length ;
+
+            for (int i = 0; i < x; i = i+2) {
+                char posi = charArray[i];
+                switch (posi) {
+                    case ('1'):
+                        spot1 = charArray[i+1];
+                        text(spot1, pos1x, pos1y);
+                        break;
+                    case ('2'):
+                        spot2 = charArray[i+1];
+                        text(spot2, pos2x, pos2y);
+                        break;
+                    case ('3'):
+                        spot3 = charArray[i+1];
+                        text(spot3, pos3x, pos3y);
+                        break;
+                    case ('4'):
+                        spot4 = charArray[i+1];
+                        text(spot4, pos4x, pos4y);
+                        break;
+                    case ('5'):
+                        spot5 = charArray[i+1];
+                        text(spot5, pos5x, pos5y);
+                        break;
+                    case ('6'):
+                        spot6 = charArray[i+1];
+                        text(spot6, pos6x, pos6y);
+                        break;
+                    case ('7'):
+                        spot7 = charArray[i+1];
+                        text(spot7, pos7x, pos7y);
+                        break;
+                    case ('8'):
+                        spot8 = charArray[i+1];
+                        text(spot8, pos8x, pos8y);
+                        break;
+                    case ('9'):
+                        spot9 = charArray[i+1];
+                        text(spot9, pos9x, pos9y);
+                        break;
+
+                }
+
+            }
+
+
+        }
+
+
     }
-
-
-    public void mousePressed(){
-        System.out.println(mouseX +" "+mouseY);
-
-
-    }
-
 
 
 
